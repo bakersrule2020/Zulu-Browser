950cfd4c2b1aed0c1e9b229a945fb250b63baa68
ThatRobloxDev <59939720+bakersrule2020@users.noreply.github.com>
2023-09-23 15:11:06 -0400

    First Update
    Adds the files and the last build.

---
+++ ConsoleWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BrowserWpf
+{
+    public partial class ConsoleWindow : Window
+    {
+        private StringBuilder consoleOutput = new StringBuilder();
+
+        public ConsoleWindow()
+        {
+            InitializeComponent();
+
+            // Redirect Console.Out to the TextBox
+            Console.SetOut(new TextBoxWriter(ConsoleTextBox, consoleOutput));
+        }
+
+        // Other methods and event handlers for your console window
+    }
+
+    // Custom TextWriter class to redirect Console output to a TextBox
+    public class TextBoxWriter : TextWriter
+    {
+        private TextBox textBox;
+        private StringBuilder outputBuffer;
+
+        public TextBoxWriter(TextBox textBox, StringBuilder outputBuffer)
+        {
+            this.textBox = textBox;
+            this.outputBuffer = outputBuffer;
+        }
+
+        public override void Write(char value)
+        {
+            // Update the TextBox and the output buffer
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                textBox.AppendText(value.ToString());
+            });
+            outputBuffer.Append(value);
+        }
+
+        public override Encoding Encoding => Encoding.UTF8;
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace BrowserWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ConsoleWindow consoleWindow = new ConsoleWindow();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            consoleWindow.Show();
+           
+            urlField.Text = View.Source.AbsoluteUri;
+            urlField.GotFocus += TextBox_GotFocus;
+            urlField.LostFocus += TextBox_LostFocus;
+            Gobutton.Click += Gobutton_Click;
+            Console.WriteLine("Initialized browser!");
+        }
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            // Add your code here to handle the closing event
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to close the browser?", "Zulu Browser | Are you sure you'd like to quit?", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            Console.WriteLine("Prompting user for close");
+            if (result == MessageBoxResult.No)
+            {
+                // Cancel the closing event if the user chooses not to close
+                e.Cancel = true;
+                Console.WriteLine("User aborted close");
+            }
+            else
+            {
+                consoleWindow.Close();
+            }
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Clear the TextBox when it receives focus
+           
+                urlField.Text = string.Empty;
+            
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // Restore the placeholder text if the TextBox is empty
+            if (string.IsNullOrWhiteSpace(urlField.Text))
+            {
+                urlField.Text = View.Source.AbsoluteUri;
+            }
+        }
+        private void Gobutton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                Uri Uriforbrowse = new Uri(urlField.Text);
+                Console.WriteLine("Going to " + urlField.Text);
+                Console.WriteLine("===URI Info===");
+                Console.WriteLine("URI Host: " + Uriforbrowse.Host);
+                Console.WriteLine("URI Path: " + Uriforbrowse.AbsolutePath);
+                Console.WriteLine("URI Query: " + Uriforbrowse.Query);
+                Console.WriteLine("==================");
+                View.Source = Uriforbrowse;
+            }
+            catch (UriFormatException ex)
+            {
+                Console.WriteLine("Error 001: InvalidFormatError\n" + ex);
+                var currentpath = Environment.CurrentDirectory;
+                Uri erroruri = new Uri("file://" + currentpath + "\\BrowserHtmls/error.html");
+                View.Source = erroruri;
+            }
+        }
+    }
+}
